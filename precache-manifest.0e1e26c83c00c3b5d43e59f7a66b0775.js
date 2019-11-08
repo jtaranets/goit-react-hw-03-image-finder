35c6a32f34a908bd47494dba96e6c125b7789587
Julia Taranets <jtaranets@gmail.com>
2019-11-08 22:07:59 +0200

    Updates

---
+++ precache-manifest.0e1e26c83c00c3b5d43e59f7a66b0775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfb11dc3bc28355835cd879230059dd",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ee6d88c2837eebf116e6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.52d79080.chunk.css"
+  },
+  {
+    "revision": "2befac4c2a0ad0b5e8b3",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a612f79a.chunk.js"
+  },
+  {
+    "revision": "ee6d88c2837eebf116e6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.56f95088.chunk.js"
+  },
+  {
+    "revision": "0a6fa8cd3e05ca9615eb",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.d5c13396.js"
+  }
+]);
